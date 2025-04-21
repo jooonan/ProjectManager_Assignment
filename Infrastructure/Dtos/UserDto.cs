2ed59349e7739a668c17e7b10dfe0484387566ec
Johan An <johanan9606@gmail.com>
2025-04-21 19:47:51 +0200

    Removed some more redundant code

--- Infrastructure/Dtos/UserDto.cs
+++ Infrastructure/Dtos/UserDto.cs
@@ -6,6 +6,4 @@ public class UserDto
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
-    public string? ConfirmPassword { get; set; }
 }
